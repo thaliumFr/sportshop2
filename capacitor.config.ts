9795d765e088da54c58b673cdea1d5c61dfdf466
ThaliumFr <theomaxmi34@gmail.com>
2025-03-31 11:36:19 +0200

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.sportshop2',
+  appName: 'sportshop2',
+  webDir: 'dist'
+};
+
+export default config;
