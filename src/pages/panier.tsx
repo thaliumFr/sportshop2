b54b2a260449781f88aff3dbe77fd07b6a85596b
ThaliumFr <theomaxmi34@gmail.com>
2025-04-24 09:44:24 +0200

    commented not working Async calls

--- src/pages/panier.tsx
+++ src/pages/panier.tsx
@@ -1,10 +1,28 @@
 import { IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './panier.css';
-import { cart } from 'ionicons/icons';
+import { cart, storefront } from 'ionicons/icons';
+// import { use, useEffect, useState } from 'react';
+// import { getProducts } from '../back/API';
 
 const Panier: React.FC = () => {
+  // let products = use(getProducts());
 
+  // const [token, setToken] = useState('');
+
+  // useEffect(() => {
+  //   // React advises to declare the async function directly inside useEffect
+  //   async function getToken() {
+  //     const data = await getProducts()
+  //     setToken(data);
+  //   };
+
+  //   // You need to restrict it at some point
+  //   // This is just dummy code and should be replaced by actual
+  //   if (!token) {
+  //     getToken();
+  //   }
+  // }, []);
 
   return (
     <IonPage>
--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -3,6 +3,8 @@ import ExploreContainer from '../components/ExploreContainer';
 import './shop.css';
 import { storefront } from 'ionicons/icons';
 import ShopItem from "../components/ShopItem";
+// import { getProducts } from "../back/API";
+// let products = getProducts()
 
 const Shop: React.FC = () => {
   return (
@@ -19,6 +21,7 @@ const Shop: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <IonContent className="ion-padding">
+          {/* {products} */}
 
           <ShopItem ItemID="2" />
         </IonContent>
