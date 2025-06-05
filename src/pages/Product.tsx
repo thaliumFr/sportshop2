3b4dfba8010ac7cb1e808deeeb44a3b3db6ba90b
ThaliumFr <theomaxmi34@gmail.com>
2025-06-05 13:36:25 +0200

    Product page

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,16 +1,25 @@
 import { IonButton, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Product.css';
 import { cart as cartIcon } from 'ionicons/icons';
-import { RouteComponentProps, withRouter } from "react-router";
 import { Cart, Item } from '../back/cart';
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { getProduct } from '../back/API';
 
 
-const Product: React.FC<RouteComponentProps> = ({ match }) => {
+
+export default function Product(props: any) {
     const { id } = useParams<{ id: string }>();
+    const [product, setProduct] = useState<Item>(new Item("", "", "", 0, 0));
     const cart = Cart.Get();
+
+    useEffect(() => {
+        getProduct(id).then((data) => {
+            setProduct(data);
+        }).catch((error) => {
+            console.error("Error fetching product:", error);
+        });
+    }, [id]);
 
     return (
         <IonPage>
@@ -26,18 +35,17 @@ const Product: React.FC<RouteComponentProps> = ({ match }) => {
                     </IonToolbar>
                 </IonHeader>
                 <IonContent className="ion-padding">
-                    <h1>{id}</h1>
+                    <h1>{product?.name}</h1>
+                    <p>{product?.desc}</p>
+                    <p>Price: {product?.price}€</p>
 
                     <IonButton onClick={() => {
-                        cart.AddProduct(new Item(id, "afeq", "sgfd", 1000, 1))
-                        cart.Save()
+                        cart.AddProduct(product ? new Item(product.reference, product.name, product.desc, product.price, 1) : new Item("", "", "", 0, 0));
                         console.log(cart.Get())
                     }}>Add to Cart</IonButton>
                 </IonContent>
             </IonContent>
         </IonPage >
     );
-};
+}
 
-
-export default Product;
